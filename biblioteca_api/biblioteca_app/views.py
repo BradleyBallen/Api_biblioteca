672d4598f0e543d3069b266542c5f47c8fc0b175
Bradley <bradleyballen162004@gmail.com>
2025-09-07 21:27:30 -0500

    installed django filters

--- biblioteca_api/biblioteca_app/views.py
+++ biblioteca_api/biblioteca_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Autor, Editorial, Libro, Miembro, Prestamo
+from .serializers import (
+    AutorSerializer, EditorialSerializer, LibroSerializer,
+    MiembroSerializer, PrestamoSerializer
+)
