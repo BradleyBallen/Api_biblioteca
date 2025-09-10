8c889f7542625b570030e6da2af0807956ceb529
Bradley <bradleyballen162004@gmail.com>
2025-09-10 14:55:21 -0500

    was corrected error of models

--- biblioteca_api/biblioteca_app/models.py
+++ biblioteca_api/biblioteca_app/models.py
@@ -28,6 +28,7 @@ class Editorial(models.Model):
 
 class Libro(models.Model):
     titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
     resumen = models.TextField()
     isbn = models.CharField(max_length=13, unique=True)
     anio_publicacion = models.PositiveIntegerField()
